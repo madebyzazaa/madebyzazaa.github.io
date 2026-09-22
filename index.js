2fc27baae695f91ddc9a0257cd35ff0416c6f6ab
madebyzazaa <madebyzazaa@gmail.com>
2026-09-22 23:55:19 +0100

    Add event listeners for form display control

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const box = document.createElement('div')
 box.className = 'box'
 document.getElementsByClassName('grid').item(0).append(box)
 }
+
+document.getElementById('add').addEventListener("click", (e)=>{document.getElementById('form').style.display='flex'; e.stopPropagation(); })
+document.addEventListener("click", ()=>{document.getElementById('form').style.display='none'})
+document.getElementById('form').addEventListener("click", (e)=>{e.stopPropagation()})
