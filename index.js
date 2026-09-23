280bb34e57bcfb1f61a671dd9b051e92f619ad20
madebyzazaa <madebyzazaa@gmail.com>
2026-09-23 01:57:23 +0100

    Refactor event listeners and add vibration support

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ document.getElementsByClassName('grid').item(0).append(box)
 document.getElementById('add').addEventListener("click", (e)=>{document.getElementById('form').style.display='flex'; e.stopPropagation(); })
 document.addEventListener("click", ()=>{document.getElementById('form').style.display='none'})
 document.getElementById('form').addEventListener("click", (e)=>{e.stopPropagation()})
+
+//streak
+//name storage
+//stats(3)
+//calendar(31/30, save&display: object)
+
+document.getElementsByClassName('backdrop').item(0).addEventListener('click', ()=>{
+// Vibrates the phone for 200 milliseconds
+  if (navigator.vibrate) {
+    navigator.vibrate(200);
+  } else {
+    console.log("Vibration API is not supported on this device/browser.");
+  }
+
+})
